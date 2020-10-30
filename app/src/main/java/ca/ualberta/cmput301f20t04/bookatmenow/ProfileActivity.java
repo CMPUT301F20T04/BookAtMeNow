c1c4433cfb9f5a06b02aa3c768d1dd3c3fb0ff05
jeannecoleongco <coleongc@ualberta.ca>
2020-10-29 19:52:49 -0600

    add new user activity

--- app/src/main/java/ca/ualberta/cmput301f20t04/bookatmenow/ProfileActivity.java
+++ app/src/main/java/ca/ualberta/cmput301f20t04/bookatmenow/ProfileActivity.java
@@ -14,12 +14,6 @@ import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.regex.Pattern;
